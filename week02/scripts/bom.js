a9367efc9890298a89ffbb954604f182e54f9029
Liam <liam@simplifybiz.com>
2024-11-14 19:34:20 -0700

    Add and Delete Inputs

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,13 +2,31 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
 
-deleteButton.textContent = "❌";
+// add chapter
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-li.append(deleteButton);
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
 
-list.append(li);
+        li.append(deleteButton);
+
+        list.append(li);
+        // Delete list item
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = "";
+        input.focus();
+    }
+});
+
+
+
+
