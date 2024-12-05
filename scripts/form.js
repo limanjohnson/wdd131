34aeec3f263c32c6b30fb957dc9743e392151c29
limanjohnson <liamjnell@gmail.com>
2024-12-05 11:57:10 -0700

    LN 1.4

--- scripts/form.js
+++ scripts/form.js
@@ -55,3 +55,21 @@ document.addEventListener("DOMContentLoaded", () => {
 function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
+
+// select DOM elements for output
+// const year = document.querySelector("#currentyear");
+
+// date object
+// const today = new Date();
+
+// year.innerhtml = 'getFullYear(): <span class="highlight">${today.getcurrentyear()}</span';
+
+const currentyear = new Date();
+let year = currentyear.getFullYear();
+document.getElementById("currentyear").innerHTML = "&copy;" + year;
+
+// alert(document.lastModified);
+//returns: day, month day, year time
+
+let oLastModif = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = oLastModif;
