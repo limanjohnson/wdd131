5b211a86bb3141c5fb22abb6cb31408cdb3ea1b1
limanjohnson <liamjnell@gmail.com>
2024-12-05 12:20:10 -0700

    LN 1.5

--- scripts/review.js
+++ scripts/review.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', ()  => {
         counterDisplay.textContent = `You have submitted ${reviewCount} reviews.`;
     }
 });
+
+
+const currentyear = new Date();
+let year = currentyear.getFullYear();
+document.getElementById("currentyear").innerHTML = "&copy;" + year;
+
+// alert(document.lastModified);
+//returns: day, month day, year time
+
+let oLastModif = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = oLastModif;
